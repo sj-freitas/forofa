5c7491081ff770ab54bd70dcacc60d7bdc395e47
Sérgio Freitas <sergiojfreitas2010@gmail.com>
2018-10-03 22:42:32 +0100

    overall refactor

--- examples/index.js
+++ examples/index.js
@@ -1,26 +1,23 @@
-const { FluentIterable } = require('./../lib');
+const { Iterable } = require('./../lib');
 const { repeat } = require('./../lib/functions');
 const { performance } = require('perf_hooks');
 
 const simpleArray = ['2', '1', '4', '3', '7', '2', '3', '99'];
-const complexArray = new FluentIterable(repeat(1000, 1))
+const complexArray = new Iterable(repeat(70000, 1))
     .map(t => Math.floor(Math.random() * 10000) + 1)
     .toArray();
 
 const lazyJs = (array) => {
-    return new FluentIterable(array)
+    return new Iterable(array)
         .map(t => parseInt(t))
         .filter(t => t >= 3)
-        .skip(1)
-        .take(5)
-        .first();
+        .toArray();
 };
 
 const eagerJs = (array) => {
     return array
         .map(t => parseInt(t))
-        .filter(t => t >= 3)
-        [0];
+        .filter(t => t >= 3);
 };
 
 console.log(lazyJs(simpleArray));
@@ -40,6 +37,6 @@ const doTest = (testId, numberOfTries, testMethod) => {
     console.log(`${testId} - completed in ${totalTime}!`);
 };
 
-const numberOfTries = 1;
+const numberOfTries = 100;
 doTest('lazy', numberOfTries, () => lazyJs(complexArray));
 doTest('eager', numberOfTries, () => eagerJs(complexArray));
