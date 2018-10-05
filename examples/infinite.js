2857d93fa07dc585d8a31d77c6501877b95147f8
Sérgio Freitas <sergiojfreitas2010@gmail.com>
2018-10-05 02:04:14 +0100

    fix all known issues

---
+++ examples/infinite.js
@@ -0,0 +1,25 @@
+const { repeat } = require("../lib/functions");
+const { Iterable } = require("../lib");
+
+const isPrime = num => {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num !== 1 && num !== 0;
+};
+
+const infiniteSequence = new Iterable(repeat(0, Infinity));
+const progressiveSequence = infiniteSequence.map((start, idx) => start + idx);
+
+// This will get the first 5 prime numbers in the sequence and
+// save it as an array. Careful to not call toArray, count or
+// any method that is supposed to get to the end of a sequence
+// on infinite sequences.
+const primes = new Iterable(progressiveSequence)
+  .filter(isPrime)
+  .take(5)
+  .toArray();
+
+console.log(primes);
